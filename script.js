0b7dcf333057114c7e0f315a0b93583e4ad46259
IHSAN ANWAR <ihsan.anwar4321@gmail.com>
2025-05-07 20:18:35 +0500

    script.js

--- script.js
+++ script.js
@@ -18,16 +18,21 @@ let isValid = true;
 
 // check username and password condition matched
     if(!usernameRegex.test(username)){
-   document.getElementById("usernameerror").textContent = " Enter a Valid Username"
+   document.getElementById("usernameerror").textContent = " *Enter a Valid Username"
    isValid = false;
     }
 
     if(!passwordRegex.test(password)){
-        document.getElementById("passworderror").textContent = " Enter a Strong Password"
+        document.getElementById("passworderror").textContent = " *Enter a Strong Password"
         isValid = false;
     }
 
-if(isValid){
-    alert("Form submitted successfully!");
+
+if (isValid) {
+    let userData = [];
+    let form = document.querySelectorAll("form input");
+    Array.from(form).forEach(currentelement => userData.push(currentelement.value));
+    Array.from(form).forEach(currentelement => { currentelement.value = ""; });
+    alert("Registered Successfully!");
 }
 });
